8d351b5a1393744e285cb1b7f52c765105b31cb5
Sabrina Link <sabrina.97.link@outlook.com>
2025-04-01 12:19:20 +0200

    create spring boot framework

---
+++ CubicFish00/src/main/java/Sabrina/Link/CubicFish/CubicFish00/CubicFish00Application.java
@@ -0,0 +1,13 @@
+package Sabrina.Link.CubicFish.CubicFish00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubicFish00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubicFish00Application.class, args);
+	}
+
+}
---
+++ CubicFish00/src/test/java/Sabrina/Link/CubicFish/CubicFish00/CubicFish00ApplicationTests.java
@@ -0,0 +1,13 @@
+package Sabrina.Link.CubicFish.CubicFish00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubicFish00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
